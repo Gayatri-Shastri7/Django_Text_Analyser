3bc5f2909e6f1eeb50789d8e44c9baddf2ab7c80
Gayatri-Shastri7 <66173499+Gayatri-Shastri7@users.noreply.github.com>
2021-05-25 11:57:10 +0530

    First commit

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index1.html')
 
     # return HttpResponse("Home")
 
@@ -18,6 +18,8 @@ def analyze(request):
     fullcaps = request.GET.get('fullcaps', 'off')
     newlineremover = request.GET.get('newlineremover', 'off')
     extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    charcount = request.GET.get('charcount', 'off')
+
 
     #Check which checkbox is on
     if removepunc == "on":
@@ -27,7 +29,38 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        return render(request, 'analyze1.html', params)
+
+    elif fullcaps=="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'analyze1.html', params)
+
+    elif newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char!="\n":
+                analyzed=analyzed+char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        return render(request, 'analyze1.html', params)
+
+
+    elif(extraspaceremover=="on"):
+            analyzed = ""
+            for index, char in enumerate(djtext):
+                if not(djtext[index] == " " and djtext[index+1]==" "):
+                    analyzed = analyzed + char
+
+            params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+            return render(request, 'analyze1.html', params)
+
+    elif charcount=="on":
+        analyzed=len(djtext)
+        params = {'purpose': 'Count no. of characters', 'analyzed_text': analyzed}
+        return render(request, 'analyze1.html', params)
+
 
     else:
         return HttpResponse("Error")
